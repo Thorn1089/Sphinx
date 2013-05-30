f5ffd0a2c16da2b26b1dd0c3490ee53ad842179b
Tom Golden <TomRK1089@gmail.com>
2013-05-29 22:20:33 -0400

    Added the AnswerListener interface, which allows the survey to react to answered questions.

---
+++ src/main/java/com/atomiccomics/survey/core/AnswerListener.java
@@ -0,0 +1,25 @@
+package com.atomiccomics.survey.core;
+
+/**
+ * <p>
+ * The {@code AnswerListener} interface allows classes to receive updates when certain questions are answered.
+ * This is useful when questions must have their visibility toggled, or new questions dynamically created from
+ * the answers to previous questions.
+ * </p>
+ * <p>
+ * Due to limitations in Java generics and type inference, implementors will have to manually cast arguments
+ * to their known types.
+ * </p>
+ * @author Tom
+ */
+public interface AnswerListener {
+
+	/**
+	 * Called when a question is answered. 
+	 * TODO Describe the registration mechanism used to narrow scope
+	 * @param question The {@link Question} which was answered by the respondent.
+	 * @param answer The {@link Answer} provided by the respondent.
+	 */
+	void answered(Question question, Answer answer);
+	
+}
