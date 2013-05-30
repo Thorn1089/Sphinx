b4c3db84da50e439ed685f44ebe82a4c29346a90
Tom Golden <TomRK1089@gmail.com>
2013-05-29 20:42:39 -0400

    Stub interface for Answers.

---
+++ src/main/java/com/atomiccomics/survey/Answer.java
@@ -0,0 +1,14 @@
+package com.atomiccomics.survey;
+
+/**
+ * <p>
+ * {@code Answer} is a marker interface used to designate certain classes as representing a respondent's
+ * input for a particular {@link Question}. There are no restrictions on the type of data that an answer
+ * can contain.
+ * </p>
+ * @author Tom
+ *
+ */
+public interface Answer {
+
+}
