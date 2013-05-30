dc1ec02bd27bc3ab04c28235782b5853f9e76cef
Tom Golden <TomRK1089@gmail.com>
2013-05-29 20:57:21 -0400

    Added package for acceptance tests.

---
+++ src/test/java/com/atomiccomics/survey/acceptance/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>
+ * The acceptance package aggregates acceptance-level tests, which are aimed at high-level features
+ * of the library. They generally demonstrate the API in action and will hopefully be a useful
+ * starting point for implementors.
+ * </p>
+ * 
+ * @author Tom
+ */
+package com.atomiccomics.survey.acceptance;
