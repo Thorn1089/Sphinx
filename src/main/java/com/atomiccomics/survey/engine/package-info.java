9b2f4b9cb797f28413044501526a4725784849d4
Tom Golden <TomRK1089@gmail.com>
2013-05-29 20:54:36 -0400

    Added engine package and basic package-info.

---
+++ src/main/java/com/atomiccomics/survey/engine/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>
+ * The engine package represents classes which drive the survey model, rendering questions and reacting
+ * to answers.
+ * </p>
+ * @author Tom
+ *
+ */
+package com.atomiccomics.survey.engine;
