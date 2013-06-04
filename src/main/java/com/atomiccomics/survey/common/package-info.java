20ac8ecb7ce3cc224e6aff7ad0dc73e8c38a40f0
Tom Golden <TomRK1089@gmail.com>
2013-06-03 21:16:07 -0400

    Added the common package.

---
+++ src/main/java/com/atomiccomics/survey/common/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>
+ * The common package contains implementations of core classes that would be expected of any
+ * reasonable survey package, such as true/false, multiple choice, and simple fill-in question and
+ * answer types.
+ * </p>
+ * 
+ * @author Tom
+ */
+package com.atomiccomics.survey.common;
