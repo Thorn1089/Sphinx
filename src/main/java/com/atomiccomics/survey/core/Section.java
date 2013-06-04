f1a0b7f51cf62cb9a682b26cd5bee6ef09e9cbbb
Tom Golden <TomRK1089@gmail.com>
2013-06-03 21:05:17 -0400

    Added the Section interface.

---
+++ src/main/java/com/atomiccomics/survey/core/Section.java
@@ -0,0 +1,42 @@
+package com.atomiccomics.survey.core;
+
+/**
+ * The {@code Section} interface defines an aggregation of related {@link Question questions}.
+ * Sections may offer additional functionality. For example, some sections may act as templates, which produce
+ * new questions dynamically at runtime based off of previous {@link Answer answers}.
+ * 
+ * @author Tom
+ */
+public interface Section {
+
+	/**
+	 * @return A {@code boolean} describing whether or not the questions in this {@link Section} should
+	 * be shown to the respondent or not.
+	 */
+	boolean isVisible();
+	
+	/**
+	 * @return True if this {@link Section} has more {@link Question#isVisible() visible} questions;
+	 * false otherwise.
+	 */
+	boolean hasNext();
+	
+	/**
+	 * @return The next {@link Question} in this section which is visible.
+	 * @throw NoSuchElementException If there are no remaining visible questions.
+	 */
+	Question next();
+	
+	/**
+	 * @return True if there are {@link Question#isVisible() visible} questions which were already returned
+	 * by this {@link Section}; false otherwise.
+	 */
+	boolean hasPrevious();
+	
+	/**
+	 * @return The {@link Question} returned by the immediately prior call to {@link #next()}.
+	 * @throw NoSuchElementException If there are no previously visible questions.
+	 */
+	Question previous();
+	
+}
