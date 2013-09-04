ce3b8a910b06edb2a6d6e8ecdde42ce69870233e
Tom Golden <TomRK1089@gmail.com>
2013-09-03 20:43:16 -0400

    Removed hasNext and hasPrevious, and next and previous methods from Section.

--- src/main/java/com/atomiccomics/survey/core/Section.java
+++ src/main/java/com/atomiccomics/survey/core/Section.java
@@ -1,5 +1,9 @@
 package com.atomiccomics.survey.core;
 
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
 /**
  * The {@code Section} interface defines an aggregation of related {@link Question questions}.
  * Sections may offer additional functionality. For example, some sections may act as templates, which produce
@@ -10,27 +14,9 @@ package com.atomiccomics.survey.core;
 public interface Section extends Visible {
 	
 	/**
-	 * @return True if this {@link Section} has more {@link Question#isVisible() visible} questions;
-	 * false otherwise.
+	 * Returns the {@code List} of children of this node in the survey; may be an empty list but never null.
+	 * @return The child nodes of this node. Can be a heterogenous mix of {@code Sections} and {@code Questions}.
 	 */
-	boolean hasNext();
-	
-	/**
-	 * @return The next {@link Question} in this section which is visible.
-	 * @throw NoSuchElementException If there are no remaining visible questions.
-	 */
-	Question next();
-	
-	/**
-	 * @return True if there are {@link Question#isVisible() visible} questions which were already returned
-	 * by this {@link Section}; false otherwise.
-	 */
-	boolean hasPrevious();
-	
-	/**
-	 * @return The {@link Question} returned by the immediately prior call to {@link #next()}.
-	 * @throw NoSuchElementException If there are no previously visible questions.
-	 */
-	Question previous();
+	@Nonnull List<Visible> children();
 	
 }
--- src/main/java/com/atomiccomics/survey/engine/SurveyDriver.java
+++ src/main/java/com/atomiccomics/survey/engine/SurveyDriver.java
@@ -1,10 +1,5 @@
 package com.atomiccomics.survey.engine;
 
-import java.util.List;
-import java.util.ListIterator;
-
-import com.atomiccomics.survey.core.Question;
-import com.atomiccomics.survey.core.Section;
 
 /**
  * The {@code SurveyDriver} wraps several concepts from the core and engine packages to present a high-level
@@ -14,51 +9,13 @@ import com.atomiccomics.survey.core.Section;
  * @author Tom
  */
 public final class SurveyDriver {
-
-	private final ListIterator<Section> sections;
-	
-	private QuestionAsker asker = new NullAsker();
-	
-	private Section current;
-	
-	public SurveyDriver(final List<Section> sections) {
-		assert(sections.size() > 0);
-		this.sections = sections.listIterator();
-		this.current = this.sections.next();
-	}
-	
-	/**
-	 * Registers the given {@link QuestionAsker} to receive updates from this {@link SurveyDriver}. Replaces
-	 * any existing asker.
-	 * @param asker A {@code QuestionAsker} which will provide interactive capabilities with the respondent.
-	 */
-	public void registerAsker(final QuestionAsker asker) {
-		this.asker = asker;
-	}
 	
 	public void next() {
-		if(current.hasNext()) {
-			asker.ask(current.next());
-		} else if(sections.hasNext()) {
-			current = sections.next();
-			next();
-		}
-		//TODO send end of survey message
+		
 	}
 	
 	public void previous() {
-		if(current.hasPrevious()) {
-			asker.ask(current.previous());
-		} else if(sections.hasPrevious()) {
-			current = sections.previous();
-			previous();
-		}
-		//TODO assertion error? Previous shouldn't be invokable at beginning
-	}
-	
-	private static class NullAsker implements QuestionAsker {
-		@Override
-		public void ask(Question question) { }
+		
 	}
 	
 }
