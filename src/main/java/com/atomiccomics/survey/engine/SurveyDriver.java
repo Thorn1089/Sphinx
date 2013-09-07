fe702cb01203b6bbd0b088cff111e16230bfa609
Tom Golden <TomRK1089@gmail.com>
2013-09-07 12:11:29 -0400

    Changed how SurveyDriver traverses questions in the survey; treats them as a tree of section nodes and question leaves.

--- src/main/java/com/atomiccomics/survey/engine/SurveyDriver.java
+++ src/main/java/com/atomiccomics/survey/engine/SurveyDriver.java
@@ -1,5 +1,14 @@
 package com.atomiccomics.survey.engine;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.ListIterator;
+
+import com.atomiccomics.survey.core.Question;
+import com.atomiccomics.survey.core.Section;
+import com.atomiccomics.survey.core.Visible;
+import com.google.common.collect.Lists;
+
 
 /**
  * The {@code SurveyDriver} wraps several concepts from the core and engine packages to present a high-level
@@ -10,12 +19,72 @@ package com.atomiccomics.survey.engine;
  */
 public final class SurveyDriver {
 	
+	private static enum Direction { FORWARD, BACKWARD }
+	
+	private final Deque<ListIterator<Visible>> stack = new ArrayDeque<>();
+	
+	private final QuestionAsker asker;
+	
+	private ListIterator<Visible> curr;
+	
+	private Question lastAsked = null;
+	
+	public SurveyDriver(final Visible root, final QuestionAsker asker) {
+		this.asker = asker;
+		curr = Lists.newArrayList(root).listIterator();
+	}
+	
 	public void next() {
-		
+		traverse(Direction.FORWARD);
 	}
 	
 	public void previous() {
-		
+		traverse(Direction.BACKWARD);
 	}
+	
+	private void traverse(Direction dir) {
+		switch(dir) {
+		case FORWARD : traverseForward(); break;
+		case BACKWARD: traverseBackward(); break;
+		default : throw new AssertionError();
+		}
+	}
+	
+	private void traverseForward() {
+		if(curr.hasNext()) {
+			visit(curr.next(), Direction.FORWARD);
+		} else {
+			curr = stack.pop();//TODO Handle end
+			traverse(Direction.FORWARD);
+		}
+	}
+	
+	private void traverseBackward() {
+		if(curr.hasPrevious()) {
+			visit(curr.previous(), Direction.BACKWARD);
+		} else {
+			curr = stack.pop();//TODO Handle end
+			traverse(Direction.BACKWARD);
+		}
+	}
+	
+	private void visit(Visible node, Direction dir) {
+		if(node != lastAsked && node.isVisible()) {
+			if(node instanceof Question) {
+				Question question = (Question)node;
+				lastAsked = question;
+				asker.ask(question);
+			} else if(node instanceof Section) {
+				Section section = (Section)node;
+				section.prepareSection();
+				stack.push(curr);
+				curr = section.children().listIterator();
+				traverse(dir);
+			}
+		} else {
+			traverse(dir);
+		}
+	}
+	
 	
 }
