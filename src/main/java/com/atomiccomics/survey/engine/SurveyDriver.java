28020f6788099773fb142c385fb593363658a3fb
Tom Golden <TomRK1089@gmail.com>
2013-09-07 12:42:53 -0400

    Added Javadocs to class.

--- src/main/java/com/atomiccomics/survey/engine/SurveyDriver.java
+++ src/main/java/com/atomiccomics/survey/engine/SurveyDriver.java
@@ -4,6 +4,8 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.ListIterator;
 
+import javax.annotation.Nonnull;
+
 import com.atomiccomics.survey.core.Question;
 import com.atomiccomics.survey.core.Section;
 import com.atomiccomics.survey.core.Visible;
@@ -13,7 +15,8 @@ import com.google.common.collect.Lists;
 /**
  * The {@code SurveyDriver} wraps several concepts from the core and engine packages to present a high-level
  * model interface for the survey as a whole, such as allowing the view layer to request updates by navigating
- * forward and back.
+ * forward and back. It also allows the host application to receive events, such as when the survey ends due
+ * to there being no more questions.
  * 
  * @author Tom
  */
@@ -31,21 +34,37 @@ public final class SurveyDriver {
 	
 	private Question lastAsked = null;
 	
-	public SurveyDriver(final Visible root, final QuestionAsker asker, final SurveyTerminationListener listener) {
+	/**
+	 * Creates a new {@link SurveyDriver} to navigate a given survey. The survey is represented in tree form, rooted
+	 * at the given {@link Visible} object.
+	 * @param root A {@code Visible} object, which is typically the top level section in the survey.
+	 * @param asker A {@link QuestionAsker} implementation, which acts as the bridge between this class and the view.
+	 * @param listener A {@link SurveyTerminationListener} which can receive a notification when the survey ends.
+	 */
+	public SurveyDriver(@Nonnull final Visible root, @Nonnull final QuestionAsker asker, 
+			@Nonnull final SurveyTerminationListener listener) {
 		this.asker = asker;
 		this.listener = listener;
 		curr = Lists.newArrayList(root).listIterator();
 	}
 	
+	/**
+	 * Provides the next question to the {@link QuestionAsker}, or fires a {@link SurveyTerminationListener#surveyEnded()}
+	 * event.
+	 */
 	public void next() {
 		traverse(Direction.FORWARD);
 	}
 	
+	/**
+	 * Provides the previously asked question to the {@link QuestionAsker}, or does nothing if at the beginning of the
+	 * survey.
+	 */
 	public void previous() {
 		traverse(Direction.BACKWARD);
 	}
 	
-	private void traverse(Direction dir) {
+	private void traverse(@Nonnull Direction dir) {
 		switch(dir) {
 		case FORWARD : traverseForward(); break;
 		case BACKWARD: traverseBackward(); break;
@@ -78,7 +97,7 @@ public final class SurveyDriver {
 		}
 	}
 	
-	private void visit(Visible node, Direction dir) {
+	private void visit(@Nonnull Visible node, @Nonnull Direction dir) {
 		if(node != lastAsked && node.isVisible()) {
 			if(node instanceof Question) {
 				Question question = (Question)node;
