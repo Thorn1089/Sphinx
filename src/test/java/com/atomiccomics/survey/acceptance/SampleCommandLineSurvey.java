3f3c23ffbf76f6ade40c737ed0907fe977474e59
Tom Golden <TomRK1089@gmail.com>
2014-04-14 22:29:43 -0400

    Added sample multiple choice and predicate logic.

--- src/test/java/com/atomiccomics/survey/acceptance/SampleCommandLineSurvey.java
+++ src/test/java/com/atomiccomics/survey/acceptance/SampleCommandLineSurvey.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import com.atomiccomics.survey.common.MultipleChoiceQuestion;
 import com.atomiccomics.survey.common.StaticSection;
 import com.atomiccomics.survey.common.TrueFalseQuestion;
 import com.atomiccomics.survey.core.Answer;
@@ -14,29 +15,36 @@ import com.atomiccomics.survey.core.Visible;
 import com.atomiccomics.survey.core.VisiblePredicate;
 import com.atomiccomics.survey.engine.SurveyBlackboard;
 import com.atomiccomics.survey.engine.SurveyDriver;
-import com.atomiccomics.survey.engine.SurveyTerminationListener;
 import com.google.common.base.Optional;
 
 public class SampleCommandLineSurvey {
 	
+	private VisiblePredicate always = () -> true;
+	
+	private Answer wrong = (obj) -> false;
+	
 	public void run() {
 		try (Scanner scanner = new Scanner(System.in)) {
-			TrueFalseQuestion first = new TrueFalseQuestion("Q-01", new AlwaysVisible(), "Is this survey awesome?");
-			TrueFalseQuestion second = new TrueFalseQuestion("Q-02", new AlwaysVisible(), "Did you lie on the previous question?", 
+			SampleBlackboard blackboard = new SampleBlackboard();
+
+			TrueFalseQuestion first = new TrueFalseQuestion("Q-01", always, "Is this survey awesome?");
+			TrueFalseQuestion second = new TrueFalseQuestion("Q-02", always, "Did you lie on the previous question?", 
 					"Yes", "No");
-			Section section = new StaticSection(new AlwaysVisible(), Arrays.<Visible> asList(first, second));
+			
+			MultipleChoiceQuestion multi = new MultipleChoiceQuestion("Q-03", always, "What is your favorite pizza?", 
+					Arrays.asList("Pepperoni", "Cheese", "Sausage"));
+			TrueFalseQuestion veggie = new TrueFalseQuestion("Q-04", () -> {
+				return blackboard.check("Q-03").or(wrong).is("Cheese");
+			}, "Are you a vegetarian?");
+			Section section = new StaticSection(always, Arrays.<Visible> asList(first, second, multi, veggie));
 			
 			CommandLineAsker asker = new CommandLineAsker();
-			asker.registerFormatter(new TrueFalseFormatter(scanner, new SampleBlackboard()));
+			asker.registerFormatter(new TrueFalseFormatter(scanner, blackboard));
+			asker.registerFormatter(new MultipleChoiceFormatter(blackboard, scanner));
 			
 			final AtomicBoolean flag = new AtomicBoolean(true);
 			
-			SurveyDriver driver = new SurveyDriver(section, asker, new SurveyTerminationListener() {
-				@Override
-				public void surveyEnded() {
-					flag.set(false);
-				}
-			});
+			SurveyDriver driver = new SurveyDriver(section, asker,() -> flag.set(false));
 			
 			while(flag.get()) {
 				driver.next();
@@ -49,16 +57,12 @@ public class SampleCommandLineSurvey {
 		survey.run();
 	}
 	
-	private static final class AlwaysVisible implements VisiblePredicate {
-		public boolean isVisible() { return true; }
-	}
-	
 	private static final class SampleBlackboard implements SurveyBlackboard	 {
 
 		private final Map<String, Answer> answers = new HashMap<>();
 		
 		@Override
-		public Optional<? extends Answer> check(String question) {
+		public Optional<Answer> check(String question) {
 			return Optional.fromNullable(answers.get(question));
 		}
 
