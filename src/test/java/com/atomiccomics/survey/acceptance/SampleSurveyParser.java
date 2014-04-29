e9f9d248519bed708ad1ef666f1728b047c7cb8c
Tom Golden <TomRK1089@gmail.com>
2014-04-28 21:46:06 -0400

    Added a demo parser and survey file.

---
+++ src/test/java/com/atomiccomics/survey/acceptance/SampleSurveyParser.java
@@ -0,0 +1,67 @@
+package com.atomiccomics.survey.acceptance;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import com.atomiccomics.SphinxLexer;
+import com.atomiccomics.SphinxParser;
+import com.atomiccomics.survey.core.Answer;
+import com.atomiccomics.survey.engine.SurveyBlackboard;
+import com.atomiccomics.survey.engine.SurveyDriver;
+import com.atomiccomics.survey.parser.SurveyBuilder;
+
+public class SampleSurveyParser {
+
+	public static void main(String[] args) throws IOException {
+		ANTLRFileStream input = new ANTLRFileStream("src/test/resources/sample.sphx");
+		SphinxLexer lexer = new SphinxLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		SphinxParser parser = new SphinxParser(tokens);
+		ParseTree tree = parser.survey();
+		
+		ParseTreeWalker walker = new ParseTreeWalker();
+		SurveyBuilder listener = new SurveyBuilder();
+		walker.walk(listener, tree);
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			final SurveyBlackboard blackboard = new SurveyBlackboard() {
+				private final Map<String, Answer> answers = new HashMap<>();
+				
+				@Override
+				public Optional<Answer> check(String question) {
+					return Optional.ofNullable(answers.get(question));
+				}
+
+				@Override
+				public void answer(String question, Answer answer) {
+					answers.put(question, answer);
+				}
+			};
+			
+			CommandLineAsker asker = new CommandLineAsker();
+			asker.registerFormatter(new TrueFalseFormatter(scanner, blackboard));
+			asker.registerFormatter(new MultipleChoiceFormatter(blackboard, scanner));
+			asker.registerFormatter(new FillInQuestionFormatter(blackboard, scanner));
+			asker.registerFormatter(new InstructionsFormatter(scanner));
+			
+			final AtomicBoolean flag = new AtomicBoolean(true);
+			
+			SurveyDriver driver = new SurveyDriver(listener.getSurvey(), asker,() -> flag.set(false));
+			
+			while(flag.get()) {
+				driver.next();
+			}
+		}
+		
+	}
+
+}
